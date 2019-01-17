ed77d9969e2457de96b9cb4afa6be1ca9cbd6621
unknown <zahra.nejadian@gmail.com>
2019-01-17 20:36:39 +0330

    database designed

--- api/DB.js
+++ api/DB.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://test1:abcd123@fdq1.ni8.ir:27017/test1');
+mongoose.connect("mongodb://127.0.0.1:17f9bfb8-90dd-44af-81bc-3d74d1e15f1f:27017/name", { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
@@ -8,3 +8,99 @@ db.once('open', function () {
 });
 
 var Schema = mongoose.Schema;
+var userSchema = new Schema({
+    userID:{ type: Schema.Types.ObjectId , required : true},
+    username: {type : String , maxlength: 20 ,required : true},
+    password :  {type : String , maxlength: 20 },
+    name : String,
+    fName : String,
+    email : String,
+    img: { data: Buffer, contentType: String },
+    Bdate : Date,
+    gender : String,
+    friends:[{
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    }]
+})
+var gameSchema = new Schema({
+    gameID: { type: Schema.Types.ObjectId , required : true},
+    clearCurrent:[],
+    winScore: Number,
+    rounds: Number,
+    dices : Number,
+    dicesInround : Number ,
+    score : Number,
+    runs: Number,
+    designer: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    }
+})
+
+var playedGamesSchema = new Schema({
+    gameID: {
+            type: Schema.Types.ObjectId,
+            ref: 'game'
+            },
+    player1: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    player2: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    score1: Number,
+    score2: Number,
+    winner : {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    loser : {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    Date : Date
+});
+
+var commentSchema = new Schema({
+    commentID : { type: Schema.Types.ObjectId , required : true},
+    gameID: {
+        type: Schema.Types.ObjectId,
+        ref: 'game'
+        },
+    playerID: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    userID: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    Date : Date,
+    comment : String
+})
+
+var rollSchema = new Schema({
+    userID: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    roll : String
+})
+
+
+var roll = mongoose.model('user', rollSchema);
+var user = mongoose.model('user', userSchema);
+var comment = mongoose.model('comment', commentSchema);
+var game = mongoose.model('game', gameSchema);
+var playedGames = mongoose.model('playedGames', playedGamesSchema);
+
+module.exports = {
+    user,
+    comment,
+    game,
+    playedGames,
+    roll
+};
--- api/index.js
+++ api/index.js
@@ -1,5 +1,21 @@
-const express = require('express');
+const express = require('express')
 const cors = require('cors')
-const app = module.exports = express();
-const bodyparser = require('body-parser');
-var mongoose = require('mongoose');
+const app = module.exports = express()
+const bodyparser = require('body-parser')
+var mongoose = require('mongoose')
+
+const {
+    user,
+    comment,
+    game,
+    playedGames,
+    roll
+} = require('./DB')
+
+app.use(cors())
+
+app.listen(3001, function () {
+    console.log('listening on 3001')
+})
+
+app.use(bodyparser.json())
